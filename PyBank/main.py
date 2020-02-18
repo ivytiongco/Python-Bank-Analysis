f09fa606744226e10109994222944dcac353aa2c
Ivy Tiongco <ivydoreen@gmail.com>
2020-02-17 19:37:37 -0800

    update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,12 +3,18 @@ import csv
 
 budgetdata = os.path.join('Desktop/PythonResources', 'budget_data.csv')
 
-total = 0
-
 with open(budgetdata) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 #    print(csvreader)
     csv_header = next(csvreader)
+    date = []
+    amount = []
+    amount_change = []
+    total = 0
+
+    for row in csvreader:
+        date.append(row[0])
+        amount.append(int(row[1]))
 
 # print headers for analysis
     print(" ")
@@ -16,22 +22,30 @@ with open(budgetdata) as csvfile:
     print("------------------------------")
 
 # total number of months included in the dataset
-    print("Total Months: " + str(len(list(csvreader))))
-# start over reading the rows
-    csvfile.seek(0)
-    next(csvreader)
+#    print("Total Months: " + str(len(list(csvreader))))
+    print("Total Months: " + str(len(date)))
 
 # net total amount of Profit/Losses
-    for row in csvreader:
-        total += int(row[1])
-    print("Total: $" + str(total))
-# start over reading the rows
-    csvfile.seek(0)
-    next(csvreader)
+    print("Total: $" + str(sum(amount)))
 
-# average of the changes in Profit/Losses over the entire period
-    for row in csvreader:
-        total += int(row[1])
-        count = len(list(csvreader))
-        avg = total/count
-    print("Average Change: $" + str(round(avg,2)))
+# find difference between rows
+    for i in range(1, len(amount)):
+        amount_change.append(amount[i] - amount[i-1])
+
+# average of difference between rows
+        avg_amount_change = sum(amount_change)/len(amount_change)
+
+# maximum of amount change is greatest increase in profits
+        max_amount_change = max(amount_change)
+
+# greatest decrease in losses (date and amount)
+        min_amount_change = min(amount_change)
+
+#find dates
+        max_amount_change_date = str(date[amount_change.index(max(amount_change))+ 1])
+        min_amount_change_date = str(date[amount_change.index(min(amount_change))+ 1])
+
+# print avg, greatest inc, greatest dec
+    print("Average Change: $" + str(round(avg_amount_change,2)))        
+    print("Greatest Increase in Profits: " + max_amount_change_date + " ($" + str(max_amount_change) + ")")
+    print("Greatest Decrease in Losses: " + min_amount_change_date + " ($" + str(min_amount_change) + ")")
