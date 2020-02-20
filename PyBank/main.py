7fd6025c3a89cd4c5f57367ef7b57c3f540cc942
Ivy Tiongco <ivydoreen@gmail.com>
2020-02-19 16:48:50 -0800

    revised main.py, added csv and txt files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,28 +1,37 @@
+# import the os module to allow us to create file paths across operating systems
 import os
+
+# module for reading CSV files
 import csv
 
-budgetdata = os.path.join('Desktop/PythonResources', 'budget_data.csv')
+# set path for file
+budgetdata = os.path.join('Desktop/Python-Bank-Poll-Analyses/PyBank', 'budget_data.csv')
 
+# open the CSV
 with open(budgetdata) as csvfile:
+    
+    # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-#    print(csvreader)
+    
+    # read the header row first
     csv_header = next(csvreader)
+    
+    # variables
     date = []
     amount = []
     amount_change = []
-    total = 0
 
+    # read each row of data after the header
     for row in csvreader:
         date.append(row[0])
         amount.append(int(row[1]))
 
-# print headers for analysis
+# print first lines of results
     print(" ")
     print("Financial Analysis")
     print("------------------------------")
 
 # total number of months included in the dataset
-#    print("Total Months: " + str(len(list(csvreader))))
     print("Total Months: " + str(len(date)))
 
 # net total amount of Profit/Losses
@@ -46,8 +55,20 @@ with open(budgetdata) as csvfile:
         min_amount_change_date = str(date[amount_change.index(min(amount_change))+ 1])
 
 # print avg, greatest inc, greatest dec
-    print("Average Change: $" + str(round(avg_amount_change,2)))        
+    print("Average Change: $" + str(round(avg_amount_change,2)))
     print("Greatest Increase in Profits: " + max_amount_change_date + " ($" + str(max_amount_change) + ")")
     print("Greatest Decrease in Losses: " + min_amount_change_date + " ($" + str(min_amount_change) + ")")
 
-# create text file of results ?????
+# set path for text file of results
+output_path = os.path.join('Desktop/Python-Bank-Poll-Analyses/PyBank', "pybank.txt")
+
+# create text file and print results to it
+f = open(output_path, 'w')
+print(" ", file = f)
+print("Financial Analysis", file = f)
+print("------------------------------", file = f)
+print("Total Months: " + str(len(date)), file = f)
+print("Total: $" + str(sum(amount)), file = f)
+print("Average Change: $" + str(round(avg_amount_change,2)), file = f)
+print("Greatest Increase in Profits: " + max_amount_change_date + " ($" + str(max_amount_change) + ")", file = f)
+print("Greatest Decrease in Losses: " + min_amount_change_date + " ($" + str(min_amount_change) + ")", file = f)
